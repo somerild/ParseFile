2b00ea5cd3042e821be7af6f3099374c93994966
somerild <somerildsolutions@gmail.com>
2022-04-06 09:35:36 -0600

    finished

--- src/fileio/ParseFile.java
+++ src/fileio/ParseFile.java
@@ -12,46 +12,73 @@ public class ParseFile {
 
         try
         {
-            if (args.length < 3) throw new RuntimeException( "Three arguments are required. " +
+            if ( args.length < 3 ) throw new IllegalArgumentException( "Three arguments are required. " +
                     "source file path, number of records, destination file path." );
-            sourcePath = args[0];
-            numberOfRecords = Integer.valueOf( args[1] );
-            destinationPath = args[2];
 
+            //set arg inputs
+            sourcePath = args[ 0 ];
+            numberOfRecords = Integer.parseInt( args[ 1 ] );
+            destinationPath = args[ 2 ];
+
+            //create file objects with source and destination inputs
             File fIn = new File( sourcePath );
-            File fOut = new File(destinationPath);
-            PrintWriter output = new PrintWriter( new FileWriter( fOut, true ) );
+            File fOut = new File( destinationPath );
 
+            //source file check
             if ( ! fIn.exists( ) )
             {
-                System.out.println("Source file does not exist." );
+                System.out.println("Source file not found." );
                 System.exit( 1 );
             }
+            if(!fIn.canRead())
+            {
+                System.out.println("Cannot read source file." );
+                System.exit( 1 );
+            }
+
             try(BufferedReader input = new BufferedReader( new FileReader( fIn ) ) )
             {
-                String record = null;
-                String state = null;
-                String population = null;
-                String childPopulation = null;
-                String childPovertyPopulation = null;
+                String record;
+                String state;
+                String population;
+                String childPopulation;
+                String childPovertyPopulation;
 
-                output.printf("%5s %10s %16s %24s%n", "State", "Population", "Child Population", "Child Poverty Population");
-
-                for(int i = 0; i < numberOfRecords; i++)
+                try(PrintWriter output = new PrintWriter( new FileWriter( fOut, true ) ))
                 {
-                    record = input.readLine();
-                    state = record.substring(0,2);
-                    population = record.substring( 82, 90 );
-                    childPopulation = record.substring( 91, 99 );
-                    childPovertyPopulation = record.substring( 100, 108 );
-                    output.printf("%5s %10s %16s %24s%n", state, population, childPopulation, childPovertyPopulation);
+                    //destination file check
+                    if(!fOut.canWrite())
+                    {
+                        System.out.println("Cannot write to destination file." );
+                        System.exit( 1 );
+                    }
+                    //print file header
+                    output.printf("%5s %10s %16s %24s%n", "State", "Population", "Child Population", "Child Poverty Population");
+
+                    //loop through records
+                    for(int i = 0; i < numberOfRecords; i++)
+                    {
+                        record = input.readLine();
+                        state = record.substring(0,2).trim();
+                        population = record.substring( 82, 90 ).trim();
+                        childPopulation = record.substring( 91, 99 ).trim();
+                        childPovertyPopulation = record.substring( 100, 108 ).trim();
+
+                        //print record to file
+                        output.printf("%5s %10s %16s %24s%n", state, population, childPopulation, childPovertyPopulation);
+                    }
                 }
-                output.close();
+            }
+            catch(IOException e1)
+            {
+                e1.printStackTrace();
+                System.exit( 1 );
             }
         }
-        catch ( Exception e )
+        catch ( IllegalArgumentException e0 )
         {
-            System.out.println(e );
+            e0.printStackTrace();
+            System.exit( 1 );
         }
     }
 }
